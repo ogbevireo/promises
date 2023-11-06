a9a6f44ae84c225fb9d5c402961e2df4720bc697
Ogbevire O., Obogrow <ogbevire@gmail.com>
2023-11-06 13:29:40 +0100

    requesting multiple data streams using fetch

--- api.js
+++ api.js
@@ -1,6 +1,14 @@
 fetch('https://swapi.dev/api/people/1')
   .then((res) => {
-    // console.log('This is resolved', res);
+    console.log('Person one resolved');
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+    return fetch('https://swapi.dev/api/people/22')
+  })
+  .then((res) => {
+    console.log('Person Two resolved');
     return res.json();
   })
   .then((data) => {
@@ -9,3 +17,4 @@ fetch('https://swapi.dev/api/people/1')
   .catch((e) => {
     console.log('Oh! Oh! you have to try again', e);
   })
+
