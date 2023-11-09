09aae95c47f017cc7427b0e9aa77b96073ebb299
Ogbevire O., Obogrow <ogbevire@gmail.com>
2023-11-09 08:47:08 +0100

    add try catch logic

--- apiwithaxios.js
+++ apiwithaxios.js
@@ -24,14 +24,17 @@ const jokes = document.querySelector('#joke-ul');
 const button = document.querySelector('button');
 
 const dadJokes = async () => {
-  const config = { headers: { Accept: 'application/json' } }
-  const res = await axios.get('https://icanhazdadjoke.com', config);
-  return res.data.joke;
+  try {
+    const config = { headers: { Accept: 'application/json' } }
+    const res = await axios.get('https://icanhazdadjoke.com', config);
+    return res.data.joke;
+  } catch (e) {
+    return `Hey you like laughing too much at Dad's expense`;
+  }
 }
 
 const newDadJoke = async () => {
   const newJoke = await dadJokes();
-  // console.log(newJoke);
   const newLi = document.createElement('li');
   newLi.append(newJoke);
   jokes.append(newLi)
